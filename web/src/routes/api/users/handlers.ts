7979d24e778d73e9e50735bbd2791e668f2a3717
Herman <german.makarkin@gmail.com>
2024-11-30 17:24:07 +0000

    implement get messages by user ids and get chats

--- web/src/routes/api/users/handlers.ts
+++ web/src/routes/api/users/handlers.ts
@@ -1,11 +1,63 @@
-import { eq, sql } from "drizzle-orm";
+import { aliasedTable, eq, sql } from "drizzle-orm";
 import { HTTPException } from "hono/http-exception";
 import db from "../../../db";
 import usersTable from "../../../db/schema/users";
+import chatsTable, { chatMessages } from "../../../db/schema/chats";
+import { userChats } from "../../../db/schema/users";
+import messagesTable from "../../../db/schema/messages"
+import { and, ne } from "drizzle-orm";
 
 export async function getUsers() {
   try {
     const result = await db.select().from(usersTable);
+    console.log(result);
+    return result;
+  } catch (e: unknown) {
+    console.log(`Error retrieving users: ${e}`);
+    return "suck";
+  }
+}
+
+export async function getChatMessages(id1: number, id2: number) {
+  try {
+    const user2 = aliasedTable(userChats, 'user2');
+    const result = await db
+      .select({
+        'id': messagesTable.id,
+        'body': messagesTable.body,
+        'notifyDate': messagesTable.notifyDate,
+        'updatedAt': messagesTable.updatedAt,
+        'createdAt': messagesTable.createdAt,
+        'createdBy': messagesTable.createdBy,
+      })
+      .from(userChats)
+      .where(eq(userChats.userId, id1))
+      .innerJoin(user2, and(eq(userChats.chatId, user2.chatId), ne(userChats.userId, user2.userId)))
+      .innerJoin(chatMessages, eq(userChats.chatId, chatMessages.chatId))
+      .innerJoin(messagesTable, eq(chatMessages.messageId, messagesTable.id))
+
+    console.log(result);
+    return result;
+  } catch (e: unknown) {
+    console.log(`Error retrieving users: ${e}`);
+    return "suck";
+  }
+}
+
+export async function getChats(id: number) {
+  try {
+    const result = await db
+      .select({
+        'id': chatsTable.id,
+        'name': chatsTable.name,
+        'desc': chatsTable.description,
+        'updatedAt': chatsTable.updatedAt,
+        'createdAt': chatsTable.createdAt,
+      })
+      .from(userChats)
+      .where(eq(userChats.userId, id))
+      .innerJoin(chatsTable, eq(userChats.chatId, chatsTable.id))
+
     console.log(result);
     return result;
   } catch (e: unknown) {
--- web/src/routes/api/users/index.ts
+++ web/src/routes/api/users/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import {
   getUsers,
+  getChats,
   getUser,
   deleteUser,
   createUser,
   updateUser,
+  getChatMessages
 } from "./handlers";
 
 const userRoutes = new Hono()
@@ -15,6 +17,14 @@ const userRoutes = new Hono()
   .get("/:id", async (c) => {
     const { id } = c.req.param();
     return c.json(await getUser(Number(id)));
+  })
+  .get("/chatsM/:userId1/:userId2", async (c) => {
+    const { userId1, userId2 } = c.req.param();
+    return c.json(await getChatMessages(Number(userId1), Number(userId2)));
+  })
+  .get("/chats/:id", async (c) => {
+    const { id } = c.req.param();
+    return c.json(await getChats(Number(id)));
   })
   .delete("/rm/:id", async (c) => {
     const { id } = c.req.param();
