0fce1399ac5b790a0fec2a9017506bde6c653b3e
Herman <german.makarkin@gmail.com>
2024-12-23 20:31:10 +0300

    add groups

---
+++ web/src/db/schema/groups.ts
@@ -0,0 +1,51 @@
+import {
+  integer,
+  pgTable,
+  primaryKey,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { userGroups } from "./users";
+import message from "./messages";
+
+const group = pgTable("groups", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }),
+  description: text("description"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const groupRelations = relations(group, ({ many }) => ({
+  messages: many(groupMessages),
+  participants: many(userGroups),
+}));
+
+export const groupMessages = pgTable(
+  "group_messages",
+  {
+    groupId: integer("group_id")
+      .notNull()
+      .references(() => group.id),
+    messageId: integer("message_id")
+      .notNull()
+      .references(() => message.id),
+  },
+  (t) => ({ pk: primaryKey({ columns: [t.groupId, t.messageId] }) }),
+);
+
+export const groupMessagesRelations = relations(groupMessages, ({ one }) => ({
+  group: one(group, {
+    fields: [groupMessages.groupId],
+    references: [group.id],
+  }),
+  message: one(message, {
+    fields: [groupMessages.messageId],
+    references: [message.id],
+  }),
+}));
+
+export default group;
--- web/src/db/schema/users.ts
+++ web/src/db/schema/users.ts
@@ -1,50 +1,78 @@
 import {
   pgTable,
-  text,
   timestamp,
   varchar,
   serial,
   integer,
   primaryKey,
+  date,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import chat from "./chats";
+import group from "./groups";
 
 const user = pgTable("users", {
   id: serial("id").primaryKey(),
+  nickname: varchar({ length: 60 }),
   name: varchar({ length: 60 }),
-  bio: text("bio"),
+  bio: varchar({ length: 80 }),
+  birthday: date("birthday"),
   phoneNumber: varchar("phone_number", { length: 15 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
 
 export const userRelations = relations(user, ({ many }) => ({
-  chats: many(userChats),
+  groups: many(userGroups),
 }));
 
-export const userChats = pgTable(
-  "user_chats",
+// export const userChats = pgTable(
+//   "user_chats",
+//   {
+//     userId: integer("user_id")
+//       .notNull()
+//       .references(() => user.id),
+//     chatId: integer("chat_id")
+//       .notNull()
+//       .references(() => chat.id),
+//   },
+//   (t) => ({ pk: primaryKey({ columns: [t.userId, t.chatId] }) }),
+// );
+
+// export const userChatsRelations = relations(userChats, ({ one }) => ({
+//   user: one(user, {
+//     fields: [userChats.userId],
+//     references: [user.id],
+//   }),
+//   chat: one(chat, {
+//     fields: [userChats.chatId],
+//     references: [chat.id],
+//   }),
+// }));
+
+export const userGroups = pgTable(
+  "user_groups",
   {
     userId: integer("user_id")
       .notNull()
       .references(() => user.id),
-    chatId: integer("chat_id")
+    groupId: integer("group_id")
       .notNull()
-      .references(() => chat.id),
+      .references(() => group.id),
   },
-  (t) => ({ pk: primaryKey({ columns: [t.userId, t.chatId] }) }),
+  (t) => ({ pk: primaryKey({ columns: [t.userId, t.groupId] }) }),
 );
 
-export const userChatsRelations = relations(userChats, ({ one }) => ({
+export const userGroupsRelations = relations(userGroups, ({ one }) => ({
   user: one(user, {
-    fields: [userChats.userId],
+    fields: [userGroups.userId],
     references: [user.id],
   }),
-  chat: one(chat, {
-    fields: [userChats.chatId],
-    references: [chat.id],
+  group: one(group, {
+    fields: [userGroups.groupId],
+    references: [group.id],
   }),
 }));
+
 
 export default user;
